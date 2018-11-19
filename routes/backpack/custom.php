9a11390a4501cc3eab8a5d1a3a5e3f1d6082ae94
Julio César Pastor Marcelo <final.africae@gmail.com>
2018-11-19 02:52:11 -0600

    Update route: deletre route default on routes/web and add routes for the CRUD  Projects, Users and Properties.

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -11,4 +11,8 @@ Route::group([
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    CRUD::resource('user', 'UserCrudController');
+    CRUD::resource('project', 'ProjectCrudController'); // user/project
+    CRUD::resource('{project_id}/properties', 'PropertyCrudController'); // user/project/property
+    Route::get('dashboard', 'ProjectCrudController@index');
 }); // this should be the absolute last line of this file
