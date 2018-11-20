f4c57288d891d548839bc6d093967c16d53f1fd9
Julio César Pastor Marcelo <final.africae@gmail.com>
2018-11-19 23:48:08 -0600

    Add  Controller and Request for update the account.

--- app/Http/Requests/AccountInfoRequest.php
+++ app/Http/Requests/AccountInfoRequest.php
@@ -1,7 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pazth
- * Date: 11/19/18
- * Time: 6:18 PM
- */
+
+namespace app\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class AccountInfoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Restrict the fields that the user can change.
+     *
+     * @return array
+     */
+    protected function validationData()
+    {
+        return $this->only(backpack_authentication_column(), 'username');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $user = backpack_auth()->user();
+
+        return [
+            backpack_authentication_column() => [
+                'required',
+                backpack_authentication_column() == 'email' ? 'email' : '',
+                Rule::unique($user->getTable())->ignore($user->getKey(),$user->getKeyName()),
+            ],
+            'username' => 'required',
+        ];
+    }
+}
